00869768854c8044888738cd9e09f5a4affc1253
Devki Nandan <110334074+DevkiNandan8@users.noreply.github.com>
2022-12-14 18:51:06 +0530

    Add files via upload

---
+++ Q1 linear search.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int linearSearch(int *arr, int x){
+    for(int i=0; i <5; i++){
+        if(arr[i] == x)
+            return i;
+        
+    }
+    return -1;
+}
+int main(){
+// assumed this list is given 
+int arr[5] = {23, 314, 45, 55, 67};
+int n;
+cout << "Enter the no to search: ";
+cin >> n;
+
+cout <<"The array, we have: " ;
+for(int i=0; i<5; i++){
+    cout << arr[i] << " ";
+}
+cout << endl;
+
+cout<<"Index position: ";
+cout<<linearSearch( arr, n) << endl;
+
+    return 0;
+    
+}
---
+++ Q2 binary search.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int *arr, int key, int l=0, int r=4){
+    
+    int mid = l+(r-l)/2;   // for the mid value
+    if(arr[mid] == key){
+        return mid;
+    }
+    if(arr[mid] < key){
+        return binarySearch(arr, key, mid+1, r);
+    }
+    if(arr[mid] > key){
+        return binarySearch(arr, key, l, mid-1);
+    }
+    return -1;
+}
+int main(){
+// assumed this ascending order list is given  
+int arr[5] = {23, 31, 45, 55, 67};
+int n;
+cout << "enter element to search: ";
+cin >> n;
+
+cout <<"The array, we have: " ;
+for(int i=0; i<5; i++){
+    cout << arr[i] << " ";
+}
+cout << endl;
+
+cout << "Index position: ";
+cout<<binarySearch( arr, n) << endl;
+
+    return 0;
+    
+}
